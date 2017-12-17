e89e840b3b86e1a08ea81ed8b880eb0d754c95b8
Nikita Trigub <nikitatr@axonian.com>
2017-12-17 17:21:41 +0200

    delete files

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -2,6 +2,7 @@ import Layout from '../components/Layout'
 import BrowseSearchBar from '../components/BrowseSearchBar'
 import Filter from '../components/Filter'
 import KittiesItem from '../components/KittiesItem'
+import Pagination from '../components/Pagination'
 
 const Marketplace = () => (
   <Layout>
@@ -20,28 +21,7 @@ const Marketplace = () => (
         </div>
       </div>
       <div className='KittiesGalleryPagination'>
-        <div className='Pagination'>
-          <div className='Pagination-pages'>
-            <button className='Pagination-page Pagination-page--active'>
-              1
-            </button>
-            <button className='Pagination-page'>2</button>
-            <button className='Pagination-page'>3</button>
-            <button className='Pagination-page'>4</button>
-            <button className='Pagination-page'>5</button>
-            <button className='Pagination-page'>6</button>
-            <button className='Pagination-page'>7</button>
-            <button className='Pagination-page'>8</button>
-            <button className='Pagination-page'>9</button>
-            <button className='Pagination-page'>10</button>
-          </div>
-          <div>
-            <button className='Pagination-button Pagination-button--disabled'>
-              Previous
-            </button>
-            <button className='Pagination-button'>Next</button>
-          </div>
-        </div>
+        <Pagination />
       </div>
     </div>
   </Layout>
